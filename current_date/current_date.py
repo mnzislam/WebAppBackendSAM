47d126ba2fbb99c235ae12ff10952d8fe48c7dc5
mnzislam <mnz.islam@gmail.com>
2024-09-22 23:13:09 -0400

    new added current date and template yaml

---
+++ current_date/current_date.py
@@ -0,0 +1,13 @@
+import json
+from datetime import datetime
+
+def lambda_handler(event, context):
+    # Get the current date and time
+    current_time = datetime.now().isoformat()
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Here is the current date and time OKOKOK',
+            'current_time': current_time
+        })
+    }
